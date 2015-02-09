2b2dca231b28b284cd429e76510d3b30dd94bad9
Fernando Garcia Samblas <fernando.gs@gmail.com>
2015-02-09 10:49:17 +0000

    Tuit#tuit_stored? in JSON index file

--- lib/auidrome/tuit.rb
+++ lib/auidrome/tuit.rb
@@ -2,29 +2,38 @@
 require 'json'
 module Auidrome
   class Tuit
-      def self.store_these tuits
-        File.open(TUITS_FILE,"w") do |f|
-          f.write JSON.pretty_generate(tuits)
-        end 
-      end
+    def self.store_these tuits
+      File.open(TUITS_FILE,"w") do |f|
+        f.write JSON.pretty_generate(tuits)
+      end 
+    end
     
-      def self.current_stored_tuits
-        if File.file?(TUITS_FILE)
-          JSON.parse(File.read(TUITS_FILE))
-        else
-          {}
-        end
-      end
+    def self.current_stored_tuits
+      @@stored_tuits ||= tuits_in_index_file
+    end
 
-      def self.read_from_index_file auido
-        {
-          auido: auido,
-          created_at: Tuit.current_stored_tuits[auido] || 'NOT FOUND!'
-        }
-      end
+    def self.tuit_stored? tuit
+      current_stored_tuits.keys.include? tuit.to_s
+    end
 
-      def self.read_json(filepath)
-        File.exists?(filepath) ? JSON.parse(File.read(filepath), symbolize_names: true) : {}
+    def self.read_from_index_file auido
+      {
+        auido: auido,
+        created_at: Tuit.current_stored_tuits[auido] || 'NOT FOUND!'
+      }
+    end
+
+    def self.read_json(filepath)
+      File.exists?(filepath) ? JSON.parse(File.read(filepath), symbolize_names: true) : {}
+    end
+
+    private
+    def self.tuits_in_index_file
+      if File.file?(TUITS_FILE)
+        JSON.parse(File.read(TUITS_FILE))
+      else
+        {}
       end
+    end
   end
 end
